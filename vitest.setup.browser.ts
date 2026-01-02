f059e476696fab633dc81f99052d552fb6d28d65
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2026-01-02 16:07:03 +0900

    Format browser test setup

--- vitest.setup.browser.ts
+++ vitest.setup.browser.ts
@@ -4,5 +4,4 @@ import { beforeEach } from "vitest";
 beforeEach(async () => {
   document.documentElement.innerHTML =
     '<head><link id="vitest-tailwind" rel="stylesheet" href="/tailwind.css"></head><body></body>';
-
 });
